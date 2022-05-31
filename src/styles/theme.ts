92a3b826f859c9cb30ca05c36048e49cabcd2362
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-30 22:47:28 -0300

    Configurando o tema do projeto.

---
+++ src/styles/theme.ts
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    background: '#E5E5E5',
+    white: '#FFFFFF',
+    black: '#000',
+    gray: {
+      '100': '#EAEFED',
+    },
+    teal: {
+      '100': '#B5F1DD',
+    },
+  },
+}
