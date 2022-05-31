56b46a8f17a74a6df0dd1a053f02681bc3d316cd
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-30 23:18:14 -0300

    tipos

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,17 +1,17 @@
-import '@emotion/react'
+import "@emotion/react";
 
-declare module '@emotion/react' {
-  export interface Theme {
+declare module "@emotion/react" {
+  export interface Theme extends LibTheme {
     colors: {
-      background: string
-      white: string
-      black: string
+      background: string;
+      white: string;
+      black: string;
       teal: {
-        '100': string
-      }
+        "100": string;
+      };
       gray: {
-        '100': string
-      }
-    }
+        "100": string;
+      };
+    };
   }
 }
