41129038f2193ac50755ed7f157b481b5ab76c0e
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-30 22:58:49 -0300

    Configurando o projeto.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,11 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
     <ThemeProvider theme={theme}>
       <Global
         styles={{
+          "*": {
+            margin: 0,
+            padding: 0,
+            "box-sizing": "border-box",
+          },
           html: {
             "@media (max-width: 1080px)": {
               "font-size": "93.75%" /*15px*/,
