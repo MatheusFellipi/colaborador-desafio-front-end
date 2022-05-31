1de26f8392f11200b5fbd33adb26165ae16cb869
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-31 19:11:25 -0300

    Criando o componente do input.

---
+++ src/components/controles/input/index.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { ReactNode } from "react";
+import { Fieldset } from "./styles";
+import { BsSearch } from "react-icons/bs";
+
+type InputType = {
+  children: ReactNode;
+};
+
+export function InputComponents() {
+  return (
+    <Fieldset>
+      <label className="input">
+        <BsSearch className="icon" />
+        <input
+          className="input__field"
+          type="text"
+          placeholder=" "
+          value="Pesquise por nome ou cpf"
+        />
+        <span className="input__label">Pesquisa por</span>
+      </label>
+    </Fieldset>
+  );
+}
