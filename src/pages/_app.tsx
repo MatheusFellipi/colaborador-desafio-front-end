2377904a4625879be7f6939011368ee3a7398d4d
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-30 22:50:54 -0300

    Configurando os estilos global.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Global, ThemeProvider } from "@emotion/react";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Global
+        styles={{
+          html: {
+            "@media (max-width: 1080px)": {
+              "font-size": "93.75%" /*15px*/,
+            },
+            "@media (max-width: 720px)": {
+              "font-size": "87.5%" /*14px*/,
+            },
+          },
+          "body, input, textarea, button": {
+            "font-family": '"Poppins",sans-serif',
+            "font-weight": "400",
+          },
+          ["disabled"]: {
+            opacity: "0.6",
+            cursor: "not-allowed",
+          },
+          button: {
+            cursor: "pointer",
+          },
+          "h1, h2, h3, h4, h5, h6, strong": {
+            "font-weight": "600",
+          },
+        }}
+      />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
