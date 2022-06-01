22a3d65e8e8807b26e7837a622d015741f1d50fe
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-01 14:31:57 -0300

    Colocando os estilos no mesmo arquivos.

--- src/components/templente/templente.tsx
+++ src/components/templente/templente.tsx
@@ -1,5 +1,5 @@
+import styled from "@emotion/styled";
 import { ReactNode } from "react";
-import { Container, Header, Heading1, Page, Wrapup } from "./styles";
 
 type TempleteType = {
   children: ReactNode;
@@ -18,3 +18,49 @@ export function Templete({ children }: TempleteType) {
     </Wrapup>
   );
 }
+
+export const Header = styled.header`
+  display: flex;
+  background: ${(props) => props.theme.colors.white};
+  width: "100%";
+  height: "3.813rem";
+  @media (min-width: 320px) {
+  }
+`;
+
+export const Aside = styled.div``;
+
+export const Page = styled.div`
+  width: 100%;
+`;
+
+export const Container = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .main {
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 40px 0px;
+    width: 96.5625%;
+
+    background: #ffffff;
+    box-shadow: 0px 4px 8px rgba(165, 171, 179, 0.16);
+    border-radius: 8px;
+    margin-bottom: 40px;
+  }
+`;
+export const Heading1 = styled.h1`
+  margin: 2.563rem 0 1.5rem 1rem;
+`;
+export const Wrapup = styled.div``;
+
+export const Divider = styled.div`
+  margin: 40px 0;
+  width: 100%;
+  height: 2px;
+  background: ${(props) => props.theme.colors.gray[100]};
+`;
