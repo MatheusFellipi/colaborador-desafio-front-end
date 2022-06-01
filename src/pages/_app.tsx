7ac2bda6d5055889414f0aa829581f61a1e3f7af
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-01 18:42:50 -0300

    Adicionando css global.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,19 +10,19 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
           "*": {
             margin: 0,
             padding: 0,
-            "box-sizing": "border-box",
+            boxSizing: "border-box",
           },
           html: {
             "@media (max-width: 1080px)": {
-              "font-size": "93.75%" /*15px*/,
+              fontSize: "93.75%" /*15px*/,
             },
             "@media (max-width: 720px)": {
-              "font-size": "87.5%" /*14px*/,
+              fontSize: "87.5%" /*14px*/,
             },
           },
           "body, input, textarea, button": {
-            "font-family": '"Poppins",sans-serif',
-            "font-weight": "400",
+            fontFamily: "Poppins,sans-serif",
+            fontWeight: "400",
           },
           body: {
             background: "#E5E5E5",
@@ -31,11 +31,30 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
             opacity: "0.6",
             cursor: "not-allowed",
           },
-          button: {
+          "button, a": {
             cursor: "pointer",
           },
           "h1, h2, h3, h4, h5, h6, strong": {
-            "font-weight": "600",
+            fontWeight: "600",
+          },
+          ".title": {
+            fontFamily: "Poppins",
+            fontStyle: "normal",
+            fontWeight: "600",
+            fontSize: "18px",
+            lineHeight: "150%",
+            color: "#34423D",
+          },
+          ".description": {
+            fontFamily: "Poppins",
+            fontStyle: "normal",
+            fontWeight: "400",
+            fontSize: "14px",
+            lineHeight: "120%",
+            color: "#587169",
+          },
+          a: {
+            textDecoration: "none",
           },
         }}
       />
