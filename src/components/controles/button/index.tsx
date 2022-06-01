e2ccf3b24ec8bbd26d5d40d0a4b05b3a9d33d4d3
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-05-31 21:42:44 -0300

    Terminando os estilos do button.

---
+++ src/components/controles/button/index.tsx
@@ -0,0 +1,41 @@
+import styled from "@emotion/styled";
+import { AiOutlineFileAdd } from "react-icons/ai";
+
+const Button_Container = styled.button`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 14px 24px;
+  width: 287px;
+  height: 52px;
+  border: 2px solid ${(props) => props.theme.colors.teal[100]};
+  background: ${(props) => props.theme.colors.white};
+  border-radius: 8px;
+
+  p {
+    width: 50px;
+    height: 24px;
+    font-family: "Poppins";
+    font-style: normal;
+    font-weight: 600;
+    font-size: 16px;
+    line-height: 150%;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    color: ${(props) => props.theme.colors.gray[600]};
+  }
+  .icon {
+    color: ${(props) => props.theme.colors.teal[100]};
+    font-size: 24px;
+  }
+`;
+
+export function Button({}) {
+  return (
+    <Button_Container>
+      <AiOutlineFileAdd className="icon" /> <p>Ações</p>
+    </Button_Container>
+  );
+}
