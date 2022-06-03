5580acba5b2867f82eac9234c24b893713c009ac
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-03 14:13:52 -0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
+import { Tabs } from "../components/tabs";
 
 import { Templete } from "../components/templente/templente";
 
 const Home: NextPage = () => {
-  return <Templete></Templete>;
+  return (
+    <Templete title="Organização" backroute={false}>
+      <Tabs />
+    </Templete>
+  );
 };
 
 export default Home;
