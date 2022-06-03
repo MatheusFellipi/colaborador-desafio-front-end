52c50ef077f8694f527c154cc6165b9f068bfeb2
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-03 14:09:54 -0300

    Update index.tsx

--- src/components/badge/index.tsx
+++ src/components/badge/index.tsx
@@ -1,19 +1,31 @@
 import styled from "@emotion/styled";
 
-const Badge_Container = styled.span`
+type BadgeContainerProps = {
+  inactive: boolean;
+};
+
+const Badge_Container = styled.span<BadgeContainerProps>`
   text-align: center;
   padding: 4px 16px;
   width: 72px;
   height: 24px;
-  background: #b5f1dd;
+  background: ${({ inactive }) => (inactive ? "#A3B8B0" : "#b5f1dd")};
+  opacity: ${({ inactive }) => (inactive ? "0.5" : "")};
   border-radius: 80px;
-  color: #34423d;
+  span {
+    color: #000000;
+  }
 `;
 
 type BadgeProps = {
   text: string;
+  inactive: boolean;
 };
 
-export function Badge({ text }: BadgeProps) {
-  return <Badge_Container>{text}</Badge_Container>;
+export function Badge({ text, inactive }: BadgeProps) {
+  return (
+    <Badge_Container inactive={inactive}>
+      <span>{text}</span>
+    </Badge_Container>
+  );
 }
