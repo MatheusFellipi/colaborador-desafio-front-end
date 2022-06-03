2df3f629b2f162fb6a00c93ac3eb0d6d5e259037
Mathues Fellipi <matheus.fellipi@outlook.com>
2022-06-03 14:13:40 -0300

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,65 +1,70 @@
 import type { AppProps } from "next/app";
+import { QueryClientProvider } from "react-query";
 import { Global, ThemeProvider } from "@emotion/react";
 import { theme } from "../styles/theme";
 
+import { queryClient } from "../services/queryClient";
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
-    <ThemeProvider theme={theme}>
-      <Global
-        styles={{
-          "*": {
-            margin: 0,
-            padding: 0,
-            boxSizing: "border-box",
-          },
-          html: {
-            "@media (max-width: 1080px)": {
-              fontSize: "93.75%" /*15px*/,
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <Global
+          styles={{
+            "*": {
+              margin: 0,
+              padding: 0,
+              boxSizing: "border-box",
             },
-            "@media (max-width: 720px)": {
-              fontSize: "87.5%" /*14px*/,
+            html: {
+              "@media (max-width: 1080px)": {
+                fontSize: "93.75%" /*15px*/,
+              },
+              "@media (max-width: 720px)": {
+                fontSize: "87.5%" /*14px*/,
+              },
             },
-          },
-          "body, input, textarea, button": {
-            fontFamily: "Poppins,sans-serif",
-            fontWeight: "400",
-          },
-          body: {
-            background: "#E5E5E5",
-          },
-          ["disabled"]: {
-            opacity: "0.6",
-            cursor: "not-allowed",
-          },
-          "button, a": {
-            cursor: "pointer",
-          },
-          "h1, h2, h3, h4, h5, h6, strong": {
-            fontWeight: "600",
-          },
-          ".title": {
-            fontFamily: "Poppins",
-            fontStyle: "normal",
-            fontWeight: "600",
-            fontSize: "18px",
-            lineHeight: "150%",
-            color: "#34423D",
-          },
-          ".description": {
-            fontFamily: "Poppins",
-            fontStyle: "normal",
-            fontWeight: "400",
-            fontSize: "14px",
-            lineHeight: "120%",
-            color: "#587169",
-          },
-          a: {
-            textDecoration: "none",
-          },
-        }}
-      />
-      <Component {...pageProps} />
-    </ThemeProvider>
+            "body, input, textarea, button": {
+              fontFamily: "Poppins,sans-serif",
+              fontWeight: "400",
+            },
+            body: {
+              background: "#E5E5E5",
+            },
+            ["disabled"]: {
+              opacity: "0.6",
+              cursor: "not-allowed",
+            },
+            "button, a": {
+              cursor: "pointer",
+            },
+            "h1, h2, h3, h4, h5, h6, strong": {
+              fontWeight: "600",
+            },
+            ".title": {
+              fontFamily: "Poppins",
+              fontStyle: "normal",
+              fontWeight: "600",
+              fontSize: "18px",
+              lineHeight: "150%",
+              color: "#34423D",
+            },
+            ".description": {
+              fontFamily: "Poppins",
+              fontStyle: "normal",
+              fontWeight: "400",
+              fontSize: "14px",
+              lineHeight: "120%",
+              color: "#587169",
+            },
+            a: {
+              textDecoration: "none",
+            },
+          }}
+        />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
